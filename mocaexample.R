086382aafa8805fdc7a72dd5a4473cd8b44c8046
PaulGinzberg <paulginzberg@hotmail.com>
2016-09-18 23:38:40 +0100

    Add files via upload

--- mocaexample.R
+++ mocaexample.R
@@ -12,16 +12,19 @@ output1 <- moca.pipeline("./example-data/prad-tcga-somatic-mutations.dat",gene.w
 
 # Extract the data files from example-data.zip into the directory /MOCA/example-data/ before running the code below
 
+# The following examples may take minutes to run.
 # Run the MOCA pipeline on the TCGA breast cancer dataset.
 output2 <- moca.pipeline("./example-data/brca-tcga-somatic-mutations.dat",gene.whitelist=gene.whitelist,save.results="./example-output/brca-example",
                          test.by.prefix=TRUE,max.merges=1e2,max.size=4)
 
-#Run the MOCA pipeline on the TCGA breast cancer dataset AND the TCGA prostate cancer dataset, combining p-values.
+# Run the MOCA pipeline on the TCGA breast cancer dataset AND the TCGA prostate cancer dataset, combining p-values.
 examplefiles <- c("./example-data/brca-tcga-somatic-mutations.dat","./example-data/prad-tcga-somatic-mutations.dat")
 output3 <- moca.pipeline(examplefiles,gene.whitelist=gene.whitelist,save.results="./example-output/brcaANDprad-example",
                          test.by.prefix=TRUE,merge.duplicates=TRUE,max.merges=1e2,max.size=10,max.p=0.01)
+# Warning messages are due to the fact that some candidate sets get an automatic p-value of 1 because
+# all alterations in the gene set are from the same gene. Ignore them safely.
 
-# # To run on all datasets combined, and reproduce our results (slow), use instead
+# # To run on all datasets combined, and reproduce our results (may take hours to run), use instead
 # examplefiles <- list.files(path="./example-data/",pattern="mutations.dat",full.names=TRUE)
 # output4 <- moca.pipeline(moca.data.files,gene.whitelist=gene.whitelist,save.results="./example-output/pancancer-test2",
 #                         test.by.prefix=TRUE,merge.duplicates=TRUE,max.merges=5e3,max.size=10,max.p=0.05,randomised=TRUE)
